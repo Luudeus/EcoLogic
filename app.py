97fdf6c6acf1e53e661d5280b17c01a537a9f587
Luu <lacabrales@gmail.com>
2023-12-01 22:31:26 -0300

    Add a connection to an example db and finish the login function

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Configure mysql.connector library to use MySQL database
+db = SQL.connect(
+    host="localhost",
+    user="user",
+    password="password",
+    database="database"
+)
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -35,3 +43,34 @@ def login():
     
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("Must provide username", "warning")
+            return render_template("login.html")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("Must provide password", "warning")
+            return render_template("login.html")
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            flash("Invalid username and/or password", "warning")
+            return render_template("login.html")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
