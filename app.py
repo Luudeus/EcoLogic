59e614a01fd5861fc302e2ce1e7402e25b33932a
Luu <lacabrales@gmail.com>
2023-12-01 21:41:48 -0300

    Add imported libraries to app.py

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
-import mysql.connector as mysql
-
+import mysql.connector as SQL
+from flask import Flask, render_template, flash, redirect, url_for, request, session
+from flask_session import Session
