b03369e2e34f8a8ed817a29fccadaf7d2b3f3360
Luu <lacabrales@gmail.com>
2023-12-01 21:44:14 -0300

    Add a configuration for the flask application and session

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import mysql.connector as SQL
 from flask import Flask, render_template, flash, redirect, url_for, request, session
 from flask_session import Session
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
