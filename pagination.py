7cb6117bd75bf6fcf6e20a1699fe3f6646c670df
Luu <lacabrales@gmail.com>
2023-12-06 19:28:58 -0300

    Translate commentaries

--- pagination.py
+++ pagination.py
@@ -6,31 +6,31 @@ class Pagination:
 
     @property
     def pages(self):
-        # Calcula el total de páginas
+        # Calculates the total pages
         return max(0, self.total_count - 1) // self.per_page + 1
 
     @property
     def has_prev(self):
-        # Verifica si hay una página anterior
+        # Checks if there's a previous page
         return self.page > 1
 
     @property
     def has_next(self):
-        # Verifica si hay una página siguiente
+        # Checks if there's a next page
         return self.page < self.pages
 
     @property
     def next_num(self):
-        # Devuelve el número de la página siguiente
+        # Returns the number of the next page
         return self.page + 1 if self.has_next else None
 
     @property
     def prev_num(self):
-        # Devuelve el número de la página anterior
+        # Returns the number of the previous page
         return self.page - 1 if self.has_prev else None
 
     def iter_pages(self, left_edge=2, left_current=2, right_current=5, right_edge=2):
-        # Itera sobre el rango de números de página para la paginación
+        # Iterates over the range of page numbers for pagination
         last = 0
         for num in range(1, self.pages + 1):
             if num <= left_edge or \
