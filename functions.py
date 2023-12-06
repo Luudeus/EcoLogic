961b837e796bcfa40656afe2c6fa44347b61f105
Luu <lacabrales@gmail.com>
2023-12-06 10:12:51 -0300

    Add function to redirect to index in case user is logged in

--- functions.py
+++ functions.py
@@ -11,3 +11,14 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+def logged_in_redirect(f):
+    """
+    Decorate routes for login and register once user is logged in.
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id"):
+            return redirect("/")
+        return f(*args, **kwargs)
+    return decorated_function
